d75a08e3e3dd1fad9bdfb925601cdbf923537fea
Unknown <Kain762@mail.ru>
2018-05-28 12:13:33 +0700

    + math Logic
    v 0.2

--- script/logicCalc.js
+++ script/logicCalc.js
@@ -2,9 +2,11 @@
 
 const calc = document.getElementById("calc");
 const display = document.getElementById("display");
+let buffer = "";
+let operation = "";
 
 calc.onclick = (event) => {
-    console.log("Target CLICK: " + event.target.id);
+    //console.log("Target CLICK: " + event.target.id);
     changeBtn(event.target.id);
 };
 
@@ -39,15 +41,26 @@ let changeBtn = (btnID) => {
         case "c":
             displayClear();
             break;
+        // математика
+        case "division":
+        case "mult":
+        case "substraction":
+        case "summ":
+            mathLog(btnID);
+            break;
+        //кнопка равно
+        case "result":
+            btnResult();
+            break;
     }
 };
 //печать чисел
 let printNum = (btnID) => {
     let numDec = btnID.substr(3);
-    console.log("printNUM: " + numDec);
+    //console.log("printNUM: " + numDec);
 
     if (displayOverflow()){
-        console.log(displayOverflow());
+        //console.log(displayOverflow());
         display.innerText += numDec;
     }
 };
@@ -63,10 +76,10 @@ let displayOverflow = () => {
 let numMark = () => {
     let textStr = display.innerText;
     if (textStr.slice(0, 1) === "-") {
-        console.log("numMark -");
+        //console.log("numMark -");
         display.innerText = textStr.slice(1);
     } else {
-        console.log("numMark +");
+        //console.log("numMark +");
         display.innerText = textStr.padStart(textStr.length + 1, "-");
     }
 };
@@ -78,8 +91,47 @@ let dotPoint = () => {
         display.innerText += ".";
     }
 };
-
 // очистка
 let displayClear = () => {
     display.innerText = "";
+};
+// математика
+let mathLog = (btnID)=> {
+    if (!(buffer && operation)) {
+        buffer = display.innerText;
+        operation = btnID;
+    } else {
+        buffer = result(buffer, display.innerText, operation);
+        console.log("result: " + buffer);
+        operation = btnID;
+    }
+    displayClear();
+    console.log("buffer: " + buffer);
+    console.log("operation: " + operation);
+};
+//транспонирование переменных
+let transform = (stringIn) => {
+    return parseFloat(stringIn);
+};
+//вычисление
+let result = (operand1, operand2, operation) => {
+    let num1 = transform(operand1);
+    let num2 = transform(operand2);
+
+    switch (operation) {
+        case "division":
+            return num1 / num2;
+        case "mult":
+            return num1 * num2;
+        case "substraction":
+            return num1 - num2;
+        case "summ":
+            return num1 + num2;
+    }
+};
+//кнопка равно
+let btnResult = () => {
+    display.innerText = result(buffer, display.innerText, operation);
+    buffer = "";
+    operation = "";
 };
